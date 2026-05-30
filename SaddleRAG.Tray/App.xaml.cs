0d07e72ed8b83f3d390d2f6d8338b4d4bd76cea5
Doug Gerard <douglas@jackalopetechnologies.com>
2026-05-30 04:22:35 -0600

    chore(tray): scaffold SaddleRAG.Tray.Core (net10.0) + SaddleRAG.Tray WPF shell

---
+++ SaddleRAG.Tray/App.xaml.cs
@@ -0,0 +1,15 @@
+// App.xaml.cs
+// Copyright © 2012–Present Jackalope Technologies, Inc. and Doug Gerard.
+// SPDX-License-Identifier: MIT
+
+#region Usings
+
+using System.Windows;
+
+#endregion
+
+namespace SaddleRAG.Tray;
+
+public partial class App : Application
+{
+}
